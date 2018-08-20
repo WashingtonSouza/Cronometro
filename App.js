7c45a13d0bbb6f22b7c6e61b390d753a559ed6a5
Washington Souza <thewashington.souza@gmail.com>
2018-08-20 00:04:51 -0300

    Criação app Cronometro

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Cronometro extends Component {
+  render() {
+    return (
+      <View style={}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
